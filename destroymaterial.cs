afcd394e3eb6de83dac51d536ef6e5db6f7825ef
Suzy Adel Wahba <128175020+SuzyAdel@users.noreply.github.com>
2025-03-23 23:05:33 -0700

    destroy(gameobj)
    can be in update and fixed update ,  not realted to the physics

---
+++ destroymaterial.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class destroymaterial : MonoBehaviour
+{
+    private float destroy_time = 5.0f;// 5 secS
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destroy_time -= Time.deltaTime;
+        if (destroy_time  <= 0)
+        {
+            Destroy(gameObject);// mynfa3sah a destroy myself fa game obj delets material
+        }
+    }
+}

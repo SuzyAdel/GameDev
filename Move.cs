91d76f9d6243f6009462625a33afac3d0d5e51d3
Suzy Adel Wahba <128175020+SuzyAdel@users.noreply.github.com>
2025-03-24 00:30:21 -0700

    added jump when grounded

--- Move.cs
+++ Move.cs
@@ -1,66 +1,98 @@
+using UnityEditor;
 using UnityEngine;
-
+// lect 4 
 public class Move : MonoBehaviour
 {
-    
-    public float speed = 10.0f; // This is the speed of the object
-    public Camera cam;  // Drag and drop main camera, and below, we'll make it follow the player
-                        //public GameObject ObjectName; // This is the object that will be seen in Unity straight away 
-                        // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 2.0f;
+    public Camera cam;
     public GameObject fallingObject;
-    private float oneSecond = 1f;
+
+    private float oneSecond = 0.1f;
+    private CharacterController characterController;
+    private bool inTriggerZone = false;//flag in zone or not
+
+    public float jumpThrust = 4.0f; // how much it goes up 
+    private float yWithGravity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        characterController = GetComponent<CharacterController>();
     }
-    // once entering the region of the collider // green plane
+
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
+        inTriggerZone = true;
+        oneSecond = 0.1f;
     }
-    // repeatedly called when the object is inside the collider
-    private void OnTriggerStay(Collider other)
-    {
-        oneSecond -= Time.deltaTime;
-        if (oneSecond <= 0f)//repeat el fall kol sec
-        {
-            oneSecond = 1f;
-            GameObject tmp = GameObject.Instantiate(fallingObject, gameObject.transform.position + new Vector3(0, 2, 0),
-                Quaternion.identity);// no rotation fa identity matrix 
-            tmp.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);// call component el renderer
-        }
-        else
-            oneSecond -= Time.deltaTime;
-    }
-    // called once when exiting 
+
     private void OnTriggerExit(Collider other)
     {
-
+        inTriggerZone = false;
     }
-   
 
-        // Update is called once per frame
-        void Update()
+    private void FixedUpdate()//called every fixed frame, used when dealing with physics, falling objects in right area
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        if (inTriggerZone)
+        {
+            if (oneSecond <= 0.0f)
+            {
+                oneSecond = 0.1f;
+                GameObject tmp = GameObject.Instantiate(fallingObject, gameObject.transform.position + new Vector3(0, 3, 0),
+                Quaternion.identity);
+                Debug.Log(gameObject.transform.position);
+                tmp.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value,
+                    Random.value);
+                // Debug.Log(other.name + " OnStay");
+            }
+            oneSecond -= Time.deltaTime;
+        }
+    }
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
         Vector3 moveVector = new Vector3(x, 0, z);
-        moveVector.Normalize(); // Normalize to prevent diagonal speed-up
-
-        transform.Translate(moveVector * speed * Time.deltaTime, Space.World);
-
-        Quaternion lookAt = Quaternion.LookRotation(moveVector, Vector3.up); // Fixed Quarternion typo
-
-        if (moveVector != Vector3.zero)
+        moveVector.Normalize();
+        // JUMP 
+        if (characterController.isGrounded)// if not the oject will fly like flappy bird 
         {
-            // transform.forward = moveVector; // The slime axis flips, so make it relative to space
-            // transform.rotation = lookAt; // Causes snapping
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookAt, 720 * Time.deltaTime); // 360 or 720 is the angular speed
+            yWithGravity = 0.0f;
 
-            // Camera follows the player correctly
-            cam.transform.position = gameObject.transform.position + new Vector3(0, 1, -5);
-            cam.transform.LookAt(gameObject.transform.position);
+            if (Input.GetButton("Jump"))// SPACE BAR automatically 
+            {
+                yWithGravity = jumpThrust; // not normilized to allow jump thrust
+            }
         }
+
+        yWithGravity += Physics.gravity.y * Time.deltaTime;// gravit max at once , so time(multiply with time to spread the effect )
+                                                           // instanteous? no so * Time.deltatime 
+
+        Vector3 moveAndJump = moveVector * speed; // move * speed
+        moveAndJump.y = yWithGravity;
+
+        characterController.Move(moveAndJump * Time.deltaTime);
+
+        //characterController.SimpleMove(moveVector * speed);
+        
+        if(moveVector != Vector3.zero)
+        {
+            Quaternion lookAt = Quaternion.LookRotation(moveVector, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookAt, 360 * Time.deltaTime);
+        }
+
+        cam.transform.position = gameObject.transform.position + new Vector3(0, 1, -5);
+        cam.transform.LookAt(gameObject.transform);
+
+
+
     }
 }

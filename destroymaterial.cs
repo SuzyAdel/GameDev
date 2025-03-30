cc65d869337027cbc9e9366dbf11931a0cf11519
Suzy Adel Wahba <128175020+SuzyAdel@users.noreply.github.com>
2025-03-30 03:16:34 -0700

    OnCollision with SPIKE destroy
    if the ball falls on the ground it will stay for 30 sec , else on spike instintly destroyed. until I figure out how to make them burst into glorious, dramatic explosions..

--- destroymaterial.cs
+++ destroymaterial.cs
@@ -1,21 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class destroymaterial : MonoBehaviour
 {
-    private float destroy_time = 5.0f;// 5 secS
+    private float destroy_time = 30.0f;// if it fell on the ground distroy after 30 sec 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         destroy_time -= Time.deltaTime;
-        if (destroy_time  <= 0)
+        if (destroy_time <= 0)
         {
             Destroy(gameObject);// mynfa3sah a destroy myself fa game obj delets material
+        }
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Spike")// if fell on the spike // shell  distroy , later will change to burst with animation 
+        {
+            Destroy(gameObject);
         }
     }
 }
